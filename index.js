fc6b6470616b9d048f7ec5c2ebd939306607d032
Mohammad Abdur Rahman <rahman.just16@gmail.com>
2021-01-29 22:58:42 +0600

    functional op.

--- index.js
+++ index.js
@@ -1,10 +1,41 @@
 document.getElementById('case-increase').addEventListener('click', function(){
-const caseInput = document.getElementById('case-count');
-const caseCount = parseInt(caseInput.value);
- const caseNewCount = caseCount + 1;
- caseInput.value = caseNewCount;
- const caseTotal = caseNewCount * 59;
- document.getElementById('case-total').innerText='$'+caseTotal;
+// const caseInput = document.getElementById('case-count');
+// const caseCount = parseInt(caseInput.value);
+//  const caseNewCount = caseCount + 1;
+//  caseInput.value = caseNewCount;
+//  const caseTotal = caseNewCount * 59;
+//  document.getElementById('case-total').innerText='$'+caseTotal;
+autoMation(true);
  
 });
  
+document.getElementById('case-decrease').addEventListener('click',function(){
+    // const caseInput = document.getElementById('case-count');
+    // const caseCount = parseInt(caseInput.value);
+    // const caseNewCount = caseCount - 1;
+    // caseInput.value = caseNewCount
+    // const caseTotal = caseNewCount * 59;
+    // document.getElementById('case-total').innerText='$'+caseTotal;
+    autoMation(false);
+
+});
+
+
+function autoMation (IsIncrease){
+            const caseInput = document.getElementById('case-count');
+            const caseCount = parseInt(caseInput.value);
+            // const caseNewCount = caseCount - 1;
+
+            let caseNewCount = caseCount;
+        if ( IsIncrease== true){
+        caseNewCount = caseCount +1
+        }
+        if (IsIncrease == false && caseCount>0){
+            caseNewCount = caseCount -1 ;
+        }
+
+
+    caseInput.value = caseNewCount
+    const caseTotal = caseNewCount * 59;
+    document.getElementById('case-total').innerText='$'+caseTotal;
+}
